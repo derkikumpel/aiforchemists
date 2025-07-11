07dfd833424eeb6369d45828a081fa5b91582cd4
derkikumpel <derkikumpel@gmail.com>
2025-07-11 10:12:24 +0200

    Update test-hf-prompt.mjs

--- scripts/test-hf-prompt.mjs
+++ scripts/test-hf-prompt.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import fetch from 'node-fetch';
 
-const HF_API_URL = 'https://api-inference.huggingface.co/models/google/flan-t5-xxl';
+const HF_API_URL = 'https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct';
 const rawOutputFile = './data/gpt-output.txt';
 
 function log(...args) {
